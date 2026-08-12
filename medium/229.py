dbf2f74027a3e87589ee37aff5fc020d691b600c
kumail-exp <kumail-exp@users.noreply.github.com>
2026-08-12 15:58:26 +0545

    solved medium -229

---
+++ medium/229.py
@@ -0,0 +1,11 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count={}
+        for val in nums:
+            count[val]=1+count.get(val,0)
+        qouta=(len(nums)//3)+1
+        out=[]
+        for val in count:
+            if(count[val]>=qouta):
+                out.append(val)
+        return out
